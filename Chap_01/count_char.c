39a894bedeba140be3b9fa84f12a43eae5e64fcd
zhunhe <xuzhunhe@gmail.com>
2021-04-18 12:28:33 +0900

    modify while loop -> for loop

--- Chap_01/count_char.c
+++ Chap_01/count_char.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
-#ifdef EOF
-#undef EOF			// already defined EOF -1 in stdio.h
-#define EOF		0	// redefine EOF -1 -> 0
-#endif
-
 int	main(void)
 {
-	long	nc;
+	double	nc;
 
 	nc = 0;
-	printf("EOF: %d", EOF);
-	while (getchar() != EOF)
-		++nc;
-	printf("nc: %ld\n", nc);
+	for (nc = 0; getchar() != EOF; ++nc)
+		;
+	printf("nc: %.0f\n", nc);
 	return (0);
 }
