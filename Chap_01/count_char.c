f8862669416763538c59227c148356b98e176b67
zhunhe <xuzhunhe@gmail.com>
2021-04-18 13:16:02 +0900

    modify count_char.c count tab, count ln, count space

--- Chap_01/count_char.c
+++ Chap_01/count_char.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
-#define CHAR_FIND		'\t'
+#define TAB_CHAR		'\t'
+#define LINEFEED_CHAR	'\n'
+#define SPACE_CHAR		' '
 
 int	main(void)
 {
 	char	c;
-	int		cnt = 0;
+	int		cnt_tab = 0;
+	int		cnt_ln = 0;
+	int		cnt_space = 0;
 	
 	while (EOF != (c = getchar()))
 	{
-		if (c == CHAR_FIND)
-			++cnt;
+		switch (c)
+		{
+			case TAB_CHAR: ++cnt_tab; break;
+			case LINEFEED_CHAR: ++cnt_ln; break;
+			case SPACE_CHAR: ++cnt_space; break;
+			default: break;
+		}
 	}
-	printf("cnt: %d\n", cnt);
+	printf("TAB: %d, LINDFEED: %d, SPACE: %d\n", cnt_tab, cnt_ln, cnt_space);
 	return (0);
 }
