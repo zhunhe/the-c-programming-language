f87e081fc09389c0ae070fbfc54fccf958eb68b9
zhunhe <xuzhunhe@gmail.com>
2021-04-18 15:12:54 +0900

    print all line and reversed line

--- Chap_01/print_longest_line.c
+++ Chap_01/print_longest_line.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAXLINE		1000
 
 int	count_line(char *s, int maxline);
 void	ft_strcpy(char *to, char *from);
+char	*reverse(char *s);
 
 int	main(void)
 {
+	int idx = 0;
 	int len;
 	int max;
 	char line[MAXLINE];
 	char longest[MAXLINE];
+	char *reversed;
 
 	max = 0;
 	while ((len = count_line(line, MAXLINE)) > 0)
@@ -20,8 +24,14 @@ int	main(void)
 			max = len;
 			ft_strcpy(longest, line);
 		}
+//		print all string and length
+		printf("string_len: %d, string: %s\n", len, line);
+//		print reversed string
+		reversed = reverse(line);
+		printf("reversed string: %s\n", reversed);
+		free(reversed);
 	}
-	printf("%s", longest);
+	printf("longest string: %s\n", longest);
 	return (0);
 }
 
@@ -38,8 +48,6 @@ int	count_line(char *s, int maxline)
 			break;
 		s[i++] = c;
 	}
-	if (c == '\n')
-		s[i++] = c;
 	s[i] = '\0';
 	return (i);
 }
@@ -48,4 +56,22 @@ void	ft_strcpy(char *to, char *from)
 {
 	while ('\0' != (*(to++) = *(from++)))
 		;
+}
+
+char	*reverse(char *s)
+{
+	char *reversed_s;
+	int len = 0;
+	int idx = 0;
+
+	while (s[len])
+		len++;
+	reversed_s = (char *)malloc(sizeof(char) * (len + 1));
+	while (s[idx] != '\0')
+	{
+		reversed_s[len - 1 - idx] = s[idx];
+		idx++;
+	}
+	reversed_s[len] = '\0';
+	return (reversed_s);
 }
