276536caf3787d38ca66a6ddb70c90c79c236bc9
zhunhe <xuzhunhe@gmail.com>
2021-04-18 13:39:09 +0900

    Init add word_count.c

---
+++ Chap_01/word_count.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int	is_blank(char c)
+{
+	return (c == ' ' || (c >= 9 && c <= 13));
+}
+
+int	main(void)
+{
+	char	c;
+	char	c_before = 0;
+	int		wd_cnt = 0;
+
+	while (EOF != (c = getchar()))
+	{
+		if ((is_blank(c_before) || c_before == 0)
+		&& (!is_blank(c)))
+			wd_cnt++;
+		c_before = c;
+	}
+	printf("count of word: %d\n", wd_cnt);
+	return (0);
+}
