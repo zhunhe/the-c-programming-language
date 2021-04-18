5138e5f2d9cd53831272a08e8ed8645976a29992
zhunhe <xuzhunhe@gmail.com>
2021-04-18 14:12:09 +0900

    add func power_recursive()

--- Chap_01/power.c
+++ Chap_01/power.c
@@ -9,9 +9,22 @@ int	power(int base, int n)
 	return (p);
 }
 
+int	power_recursive(int base, int n)
+{
+	if (n < 0)
+		return (0);
+	if (n == 0 || base == 1)
+		return (1);
+	return (base * power_recursive(base, n - 1));
+}
+
 int	main(void)
 {
 	for (int i = 0; i < 10; i++)
-		printf("power(2, %d): %3d, power(-3, %d): %6d\n", i, power(2, i), i, power(-3, i));
+		printf("power(2, %d): %3d, power(-3, %d): %6d\n"
+				, i, power(2, i), i, power(-3, i));
+	for (int i = 0; i < 10; i++)
+		printf("power_recursive(2, %d): %3d, power_recursive(-3, %d): %6d\n"
+				, i, power_recursive(2, i), i, power_recursive(-3, i));
 	return (0);
 }
