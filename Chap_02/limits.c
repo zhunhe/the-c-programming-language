f7ca1dfe4886878bebbfcfa69cdd3d0920c9a4b7
zhunhe <xuzhunhe@gmail.com>
2021-04-18 17:01:41 +0900

    Init add limits.c

---
+++ Chap_02/limits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <limits.h>
+
+int	main(void)
+{
+	printf("CHAR_MIN: %d\n", CHAR_MIN);
+	printf("CHAR_MAX: %d\n", CHAR_MAX);
+	printf("SHORT_MIN: %d\n", SHRT_MIN);
+	printf("SHORT_MAX: %d\n", SHRT_MAX);
+	printf("INT_MIN: %d\n", INT_MIN);
+	printf("INT_MAX: %d\n", INT_MAX);
+	printf("LONG_MIN: %ld\n", LONG_MIN);
+	printf("LONG_MAX: %ld\n", LONG_MAX);
+	return (0);
+}
