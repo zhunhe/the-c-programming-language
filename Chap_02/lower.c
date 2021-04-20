6a98ce22ec51fc53baf47b40374906cca0e11e78
zhunhe <xuzhunhe@gmail.com>
2021-04-20 13:14:02 +0900

    Modify lower() using conditional expression

--- Chap_02/lower.c
+++ Chap_02/lower.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <ctype.h>
+
+/*
+** Exercise 2-10.
+** Write the function lower,
+** which converts upper case letters to lower cases,
+** with a conditional expression instead of if-else.
+*/
 
 void	print_to_lowercase(char *s)
 {
@@ -7,9 +14,11 @@ void	print_to_lowercase(char *s)
 
 	while (*s)
 	{
-		c = tolower(*(s++));
+		c = (*s >= 'A' && *s <= 'Z' ? *s - 'A' + 'a': *s);
 		write(1, &c, 1);
+		s++;
 	}
+	write(1, "\n", 1);
 }
 
 int	main(int argc, char **argv)
@@ -18,10 +27,7 @@ int	main(int argc, char **argv)
 	{
 		argv++;
 		while (*argv)
-		{
 			print_to_lowercase(*(argv++));
-			write(1, "\n", 1);
-		}
 	}
 	return (0);
 }
