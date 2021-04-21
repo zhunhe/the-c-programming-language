8ffbdbfbe2279f518f54826eeae59b7e52942665
zhunhe <xuzhunhe@gmail.com>
2021-04-21 18:45:22 +0900

    Add comment

--- Chap_03/itoa.c
+++ Chap_03/itoa.c
@@ -2,6 +2,19 @@
 #include <string.h>
 #include <limits.h>
 
+/*
+** Exercise 3-4.
+** In a two's complement number representation,
+** our version of itoa does not handle the largest negative number,
+** that is, the value of n equal to -(2^wordsize-1).
+** Explain why not. Modify it to print that value correctly,
+** regardless of the machine on which it runs.
+** 
+** Answer) Because INT_MIN's two's complement is INT_MIN
+** 10000.....00000(INT_MIN) 's one's complement is 01111.....11111
+** 01111.....11111 plus one is 10000.....00000
+** The way to handle INT_MIN is casting it to a larger data type such as long long int.
+*/
 void	reverse(char *s)
 {
 	char tmp;
